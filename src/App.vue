dfbc1abe2e056c2fa109fdcb03dba4427d21d80d
Alireza Safari <alireza.safaree@gmail.com>
2021-07-15 09:38:59 +0430

    Removed pwa/router/vuex from dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-      <router-view/>
+      <Home />
   </div>
 </template>
 
 <script>
+import Home from './views/Home.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Home
+  }
 }
 </script>
 
