44de3dba3b2b740fbccf060fa5526b3e87f3d91c
Alireza Safari <alireza.safaree@gmail.com>
2022-03-15 15:29:55 +0330

    set default port to 8080

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-const path = require("path");
+const path = require("path")
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8080
+  },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
