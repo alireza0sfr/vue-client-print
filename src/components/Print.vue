4684c9169b0712c0529ff4396fd92872ec545a8e
Alireza Safari <alireza.safaree@gmail.com>
2022-03-16 10:51:54 +0330

    moved images to assets

--- src/components/Print.vue
+++ src/components/Print.vue
@@ -27,10 +27,10 @@
 					</div>
 					<div style="display: flex">
 						<a @click="editWhileInPreview()" title="ویرایش" class="modal-icon" href="#">
-							<img src="./elements/images/edit.png" />
+							<img src="../assets/images//edit.png" />
 						</a>
 						<a href="#" @click="printForm()" title="چاپ" class="modal-icon">
-							<img src="./elements/images/printer.png" />
+							<img src="../assets/images//printer.png" />
 						</a>
 					</div>
 				</div>
