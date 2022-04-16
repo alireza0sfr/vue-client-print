bee207e7d809191b9ce92d802d543df19b64d72e
Alireza Safari <alireza.safaree@gmail.com>
2022-04-16 12:24:03 +0430

    added resizer component

---
+++ src/components/elements/Resizers.vue
@@ -0,0 +1,29 @@
+<template>
+	<div v-for="area in cleanedResizeables" :key="area" :class="`elem-resizer ${area}`"></div>
+</template>
+
+<script>
+	export default {
+		props: {
+			resizers: { type: Array, default: ['br'] }
+		},
+		watch: {
+			resizers: {
+				immediate: true,
+				deep: true,
+				handler(val) {
+					if (val && val.length)
+						this.resizer = val
+				},
+			},
+		},
+		computed: {
+			cleanedResizeables() {
+				return this.resizers.filter(x => x && x.length > 1)
+			},
+		},
+	}
+</script>
+
+<style>
+</style>
--- src/plugins/element-utilities.js
+++ src/plugins/element-utilities.js
@@ -1,133 +1,136 @@
-function resizable(element) {
-    const resizers = document.getElementsByClassName('elem-resizer')
-
-    for (let resizer of resizers) {
-        resizer.onmousedown = (e) => initDrag(e, resizer)
+class Element {
+    constructor(element) {
+        this.element = element
     }
 
-    element.onmousedown = dragable
-    var startX, startY, startWidth, startHeight, originalLeft, originalTop
-    function initDrag(e, resizer) {
-        if (e.target.className.includes('elem-resizer')) { // If Clicking on the resizer
-            originalLeft = element.offsetLeft
-            originalTop = element.offsetTop
-            startX = e.clientX
-            startY = e.clientY
-            startWidth = parseInt(document.defaultView.getComputedStyle(element).width, 10)
-            startHeight = parseInt(document.defaultView.getComputedStyle(element).height, 10)
-            document.onmousemove = (e) => doDrag(e, resizer)
-            document.onmouseup = stopDrag
-        }
-    }
-    function doDrag(e, resizer) {
-        if (resizer.className.includes('right'))
-            element.style.width = startWidth + e.clientX - startX + "px"
+    resizable(element = this.element) {
+        const resizers = document.getElementsByClassName('elem-resizer')
 
-        if (resizer.className.includes('top')) {
-            element.style.height = startY - (e.clientY - startHeight) + "px"
-            element.style.top = originalTop + (e.clientY - startY) + 'px'
+        for (let resizer of resizers) {
+            resizer.onmousedown = (e) => initDrag(e, resizer)
         }
 
-        if (resizer.className.includes('bottom'))
-            element.style.height = startHeight + e.clientY - startY + "px"
+        element.onmousedown = this.dragable
+        var startX, startY, startWidth, startHeight, originalLeft, originalTop
+        function initDrag(e, resizer) {
+            if (e.target.className.includes('elem-resizer')) { // If Clicking on the resizer
+                originalLeft = element.offsetLeft
+                originalTop = element.offsetTop
+                startX = e.clientX
+                startY = e.clientY
+                startWidth = parseInt(document.defaultView.getComputedStyle(element).width, 10)
+                startHeight = parseInt(document.defaultView.getComputedStyle(element).height, 10)
+                document.onmousemove = (e) => doDrag(e, resizer)
+                document.onmouseup = stopDrag
+            }
+        }
+        function doDrag(e, resizer) {
+            if (resizer.className.includes('right'))
+                element.style.width = startWidth + e.clientX - startX + "px"
 
-        if (resizer.className.includes('left')) {
-            element.style.width = startWidth - e.clientX + startX + "px"
-            element.style.left = originalLeft + (e.clientX - startX) + 'px'
+            if (resizer.className.includes('top')) {
+                element.style.height = startY - (e.clientY - startHeight) + "px"
+                element.style.top = originalTop + (e.clientY - startY) + 'px'
+            }
+
+            if (resizer.className.includes('bottom'))
+                element.style.height = startHeight + e.clientY - startY + "px"
+
+            if (resizer.className.includes('left')) {
+                element.style.width = startWidth - e.clientX + startX + "px"
+                element.style.left = originalLeft + (e.clientX - startX) + 'px'
+            }
+
+            if (resizer.className.includes('br')) {
+                element.style.width = startWidth + e.clientX - startX + "px"
+                element.style.height = startHeight + e.clientY - startY + "px"
+            }
+
+            if (resizer.className.includes('tr')) {
+                element.style.width = startWidth + e.clientX - startX + "px"
+                element.style.height = startY - (e.clientY - startHeight) + "px"
+                element.style.top = originalTop + (e.clientY - startY) + 'px'
+            }
+
+            if (resizer.className.includes('bl')) {
+                element.style.width = startWidth - e.clientX + startX + "px"
+                element.style.height = startHeight + e.clientY - startY + "px"
+                element.style.left = originalLeft + (e.clientX - startX) + 'px'
+            }
+
+            if (resizer.className.includes('tl')) {
+                element.style.height = startY - (e.clientY - startHeight) + "px"
+                element.style.width = startWidth - e.clientX + startX + "px"
+                element.style.left = originalLeft + (e.clientX - startX) + 'px'
+                element.style.top = originalTop + (e.clientY - startY) + 'px'
+            }
         }
 
-        if (resizer.className.includes('br')) {
-            element.style.width = startWidth + e.clientX - startX + "px"
-            element.style.height = startHeight + e.clientY - startY + "px"
-        }
-
-        if (resizer.className.includes('tr')) {
-            element.style.width = startWidth + e.clientX - startX + "px"
-            element.style.height = startY - (e.clientY - startHeight) + "px"
-            element.style.top = originalTop + (e.clientY - startY) + 'px'
-        }
-
-        if (resizer.className.includes('bl')) {
-            element.style.width = startWidth - e.clientX + startX + "px"
-            element.style.height = startHeight + e.clientY - startY + "px"
-            element.style.left = originalLeft + (e.clientX - startX) + 'px'
-        }
-
-        if (resizer.className.includes('tl')) {
-            element.style.height = startY - (e.clientY - startHeight) + "px"
-            element.style.width = startWidth - e.clientX + startX + "px"
-            element.style.left = originalLeft + (e.clientX - startX) + 'px'
-            element.style.top = originalTop + (e.clientY - startY) + 'px'
-        }
-    }
-
-    function stopDrag(e) {
-        document.onmousemove = null
-        document.onmouseup = null
-        element.dispatchEvent(new Event("finishededitingelement"))
-    }
-}
-
-function click(element) {
-    element.addEventListener("mousedown", onClick, false)
-    function onClick() {
-        let selectedElements = document.getElementsByClassName('element selected')
-        for (let index = 0; index < selectedElements.length; index++) {
-            selectedElements[index].classList.remove('selected')
-        }
-        element.classList.add('selected')
-    }
-
-}
-
-function dragable(element, classType) {
-
-    var pos1 = 0,
-        pos2 = 0,
-        pos3 = 0,
-        pos4 = 0
-
-    // move the DIV from anywhere inside the DIV:
-    element.onmousedown = dragMouseDown
-
-    function dragMouseDown(e) {
-        if (
-            e.target.className.includes('element') ||
-            e.target.offsetParent.className.includes('element') &&
-            e.target.className === 'image') { // if dragging element or image element/variable
-            e = e || window.event
-            e.preventDefault()
-            // get the mouse cursor position at startup:
-            pos3 = e.clientX
-            pos4 = e.clientY
-            document.onmouseup = closeDragElement
-            // call a function whenever the cursor moves:
-            document.onmousemove = elementDrag
-        }
-
-        function elementDrag(e) {
-            e = e || window.event
-            e.preventDefault()
-            // calculate the new cursor position:
-            pos1 = pos3 - e.clientX
-            pos2 = pos4 - e.clientY
-            pos3 = e.clientX
-            pos4 = e.clientY
-            // set the element's new position:
-            element.style.top = element.offsetTop - pos2 + "px"
-            element.style.left = element.offsetLeft - pos1 + "px"
-        }
-
-        function closeDragElement() {
-            // stop moving when mouse button is released:
-            document.onmouseup = null
+        function stopDrag(e) {
             document.onmousemove = null
+            document.onmouseup = null
             element.dispatchEvent(new Event("finishededitingelement"))
         }
     }
+
+    click(element = this.element) {
+        element.addEventListener("mousedown", onClick, false)
+        function onClick() {
+            let selectedElements = document.getElementsByClassName('element selected')
+            for (let index = 0; index < selectedElements.length; index++) {
+                selectedElements[index].classList.remove('selected')
+            }
+            element.classList.add('selected')
+        }
+
+    }
+
+    dragable(element = this.element) {
+
+        var pos1 = 0,
+            pos2 = 0,
+            pos3 = 0,
+            pos4 = 0
+
+        // move the DIV from anywhere inside the DIV:
+        element.onmousedown = dragMouseDown
+
+        function dragMouseDown(e) {
+            if (
+                e.target.className.includes('element') ||
+                e.target.offsetParent.className.includes('element') &&
+                e.target.className === 'image') { // if dragging element or image element/variable
+                e = e || window.event
+                e.preventDefault()
+                // get the mouse cursor position at startup:
+                pos3 = e.clientX
+                pos4 = e.clientY
+                document.onmouseup = closeDragElement
+                // call a function whenever the cursor moves:
+                document.onmousemove = elementDrag
+            }
+
+            function elementDrag(e) {
+                e = e || window.event
+                e.preventDefault()
+                // calculate the new cursor position:
+                pos1 = pos3 - e.clientX
+                pos2 = pos4 - e.clientY
+                pos3 = e.clientX
+                pos4 = e.clientY
+                // set the element's new position:
+                element.style.top = element.offsetTop - pos2 + "px"
+                element.style.left = element.offsetLeft - pos1 + "px"
+            }
+
+            function closeDragElement() {
+                // stop moving when mouse button is released:
+                document.onmouseup = null
+                document.onmousemove = null
+                element.dispatchEvent(new Event("finishededitingelement"))
+            }
+        }
+    }
+
 }
-export default {
-    resizable,
-    dragable,
-    click
-}
+export default Element
