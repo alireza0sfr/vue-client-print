5b32c8ba4849a462bfa0217204f57cec8b616ecc
Alireza Safari <alireza.safaree@gmail.com>
2022-05-08 11:04:52 +0430

    feat(mixins): add deepmerge && isObject methods

--- src/plugins/mixins.ts
+++ src/plugins/mixins.ts
@@ -8,6 +8,44 @@ var mixins: object = {
     idGenerator(n: number): string {
       return Math.random().toString(36).substr(2, n)
     },
+
+    /**
+     * Check weather given item is object.
+     * @param {any} item - given item to check.
+     * @return {Boolean} - true if item is object.
+     */
+    isObject(item: any): boolean {
+      return (item && typeof item === 'object' && !Array.isArray(item))
+    },
+
+    /**
+     * Deep merge sources to target object recuresively.
+     * @param {Object} target - Target object.
+     * @param {Array} sources - Array of sources.
+     * @return {Object} - Merged object.
+     */
+    merge(target: object, ...sources: object[]): object {
+
+      if (!sources.length) return target
+      const source = sources.shift()
+
+      if (this.isObject(target) && this.isObject(source)) {
+        for (const key in source) {
+          if (this.isObject(source[key])) {
+            if (!target[key]) Object.assign(target, {
+              [key]: {}
+            })
+            this.merge(target[key], source[key])
+          } else {
+            Object.assign(target, {
+              [key]: source[key]
+            })
+          }
+        }
+      }
+
+      return this.merge(target, ...sources)
+    }
   }
 }
 
