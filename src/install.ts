dc36ff5e309c071fb7b28595a2595d54c7ccd01e
Alireza Safari <alireza.safaree@gmail.com>
2022-05-26 09:59:48 +0430

    build(package): updates version to 0.9.933
    adds repository property to package json & export components
    individually

--- src/install.ts
+++ src/install.ts
@@ -1,8 +1,12 @@
 // Import vue component
-import Print from './components/Print.vue'
+import Print from '~/components/Print.vue'
+import TemplateBuilder from '~/components/TemplateBuilder.vue'
 
 export default {
   install: (app: any, options: any): void => {
     app.component('Print', Print)
+    app.component('TemplateBuilder', TemplateBuilder)
   }
 }
+
+export { Print, TemplateBuilder }
