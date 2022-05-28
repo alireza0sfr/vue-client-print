f59a0c55402f6f44c0dc70124d2e913e5ed1f8e0
Alireza Safari <alireza.safaree@gmail.com>
2022-05-28 14:21:04 +0430

    build(package): updates to version 0.9.953

--- src/install.ts
+++ src/install.ts
@@ -1,12 +1,23 @@
 // Import vue component
 import Print from '~/components/Print.vue'
 import TemplateBuilder from '~/components/TemplateBuilder.vue'
+import { createI18n } from 'vue-i18n'
+import translations from './assets/translations'
+
+const i18n = createI18n({
+  locale: 'fa',
+  fallbackLocale: 'en',
+  // @ts-ignore
+  messages: translations
+})
+
 
 export default {
   install: (app: any, options: any): void => {
     app.component('Print', Print)
     app.component('TemplateBuilder', TemplateBuilder)
+    app.use(i18n)
   }
 }
 
-export { Print, TemplateBuilder }
+export { Print, TemplateBuilder, i18n as $t }
