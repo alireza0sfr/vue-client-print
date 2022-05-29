7b4a057d1f36cc5e3c53f3988032ba2281e780cf
Alireza Safari <alireza.safaree@gmail.com>
2022-05-29 13:20:00 +0430

    feat(i18n): adds a seprate i18n initializer

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import { createApp } from 'vue'
-// import { createI18n } from 'vue-i18n'
-import { createI18n } from 'vue-i18n/index'
 import App from './App.vue'
-import translations from './assets/translations'
-import mixins from './plugins/mixins'
+// @ts-ignore
+import i18n from './plugins/i18n.ts'
+// @ts-ignore
+import mixins from './plugins/mixins.ts'
 // @ts-ignore
 import componentRegisterer from './plugins/components.ts'
-
-const i18n = createI18n({
-  locale: 'fa',
-  fallbackLocale: 'en',
-  // @ts-ignore
-  messages: translations
-})
 
 const app = createApp(App)
 app.mixin(mixins)
