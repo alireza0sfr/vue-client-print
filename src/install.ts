b718ea7d0d9e0a1b91f48546066955705f1991d5
Alireza Safari <alireza.safaree@gmail.com>
2022-05-30 11:51:40 +0430

    build(package): updates to version 0.9.962
    changes:
    - add unit test files
    - add coverage to unit tests
    - add i18n instance to install.ts

--- src/install.ts
+++ src/install.ts
@@ -3,12 +3,15 @@ import Print from '~/components/Print.vue'
 import TemplateBuilder from '~/components/TemplateBuilder.vue'
 // @ts-ignore
 import mixins from '../src/plugins/mixins.ts'
+// @ts-ignore
+import i18n from '../src/plugins/i18n.ts'
 
 export default {
   install: (app: any, options: any): void => {
     app.component('Print', Print)
     app.component('TemplateBuilder', TemplateBuilder)
     app.mixin(mixins)
+    app.use(i18n)
   }
 }
 
