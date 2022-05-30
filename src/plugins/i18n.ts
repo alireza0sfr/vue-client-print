0713363bd7c08482562f662dede765e96486e7bd
Alireza Safari <alireza.safaree@gmail.com>
2022-05-30 12:35:44 +0430

    build(updes to version 0.9.964): used .json extenstion instead of .ts for i18n

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n'
 // @ts-ignore
-import translations from '../assets/translations.ts'
+import translations from '../assets/translations.json'
 const i18n = createI18n({
   locale: 'fa',
   fallbackLocale: 'en',
