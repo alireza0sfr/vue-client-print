c3cc640e436a6a12b619988ba18c077c2d7f836a
Alireza Safari <alireza.safaree@gmail.com>
2022-05-30 10:35:27 +0430

    build(package): updaes to version 0.9.961
    adds mixins to install

--- src/install.ts
+++ src/install.ts
@@ -1,11 +1,14 @@
 // Import vue component
 import Print from '~/components/Print.vue'
 import TemplateBuilder from '~/components/TemplateBuilder.vue'
+// @ts-ignore
+import mixins from '../src/plugins/mixins.ts'
 
 export default {
   install: (app: any, options: any): void => {
     app.component('Print', Print)
     app.component('TemplateBuilder', TemplateBuilder)
+    app.mixin(mixins)
   }
 }
 
