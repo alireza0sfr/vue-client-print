9c57f9b845aca3754272d12ef03843689b7309a7
Alireza Safari <alireza.safaree@gmail.com>
2022-05-30 13:39:54 +0430

    build(package): updaes to version 0.9.968
    adds componentRegisterer to install method

--- src/install.ts
+++ src/install.ts
@@ -2,6 +2,8 @@
 import Print from '~/components/Print.vue'
 import TemplateBuilder from '~/components/TemplateBuilder.vue'
 // @ts-ignore
+import componentRegisterer from './plugins/components.ts'
+// @ts-ignore
 import mixins from './plugins/mixins.ts'
 // @ts-ignore
 // import i18n from './plugins/i18n.ts'
@@ -11,6 +13,7 @@ export default {
     app.component('Print', Print)
     app.component('TemplateBuilder', TemplateBuilder)
     app.mixin(mixins)
+    componentRegisterer(app)
     // app.use(i18n)
   }
 }
