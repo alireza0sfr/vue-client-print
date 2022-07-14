0a92bf1a9196c6e57d94db97b3605668ef017606
Alireza Safari <alireza.safaree@gmail.com>
2022-07-14 12:38:35 +0430

    ci(docs): fixes deploy gh pages ci bugs

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -9,7 +9,7 @@ npm install
 npm run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd src/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -18,8 +18,9 @@ git init
 git checkout -b gh-pages
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:<USERNAME>/<REPO>.git gh-pages:gh-pages
+git push -f git@github.com:alireza0sfr/vue-client-print.git gh-pages:gh-pages
 git checkout main
 git branch -D gh-pages
 cd -
 rm -rf docs/.vuepress/dist
+rm -rf docs/.vuepress/dist/.git
