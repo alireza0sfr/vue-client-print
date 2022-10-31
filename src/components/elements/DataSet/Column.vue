fcc7db24edb4c5f1ee1e285bb6a2dffc57939575
Alireza Safari <alireza.safaree@gmail.com>
2022-10-31 13:19:37 +0330

    refactor(column): refactor column to support new element class)

--- src/components/elements/DataSet/Column.vue
+++ src/components/elements/DataSet/Column.vue
@@ -1,50 +1,38 @@
 <template>
-	<div :id="settings.id" ref="element" :class="locals.classType + settings.class + ' element label'" :style="settings.styles">
-		<span>{{settings.configs.title}}</span>
-		<Resizers v-if="settings.hasResizer" :query="`column-${settings.id}`" :resizers="['right']"/>
+	<div :id="element.id" ref="element" :class="element.type + element.class + ' element label'" :style="element.styles">
+		<span>{{element.configs.title}}</span>
+		<Resizers v-if="element.hasResizer" :query="`${element.type}-${element.id}`" :resizers="['right']" />
 	</div>
 </template>
 
-<script>
+<script lang="ts">
+	import { ElementGrandParents, ElementTypes } from '~/enums/element'
+	import { IElement } from '~/interfaces/elements'
 	import { merge } from '~/plugins/general-utilities'
-	import ElementClass, { initElementStyles } from '~/plugins/element-utilities.ts'
-	export default {
-		name: "Column",
+	import { defineComponent } from 'vue'
+	export default defineComponent({
+		name: ElementTypes.COLUMN,
 		props: {
-			options: Object,
+			instance: Object as () => IElement,
 		},
-		emits:['width-changed'],
+		emits: ['width-changed'],
 		mounted() {
-			if (this.settings.grandParent === "TemplateBuilder") { // Initialize on moutned if its the template builder mode
+			if (this.element.grandParent === ElementGrandParents.TEMPLATEBUILDER)
 				this.initialize()
-			}
 		},
 		watch: {
-			options: {
+			instance: {
 				immediate: true,
-				deep: true,
 				handler(val) {
-					this.settings = merge(this.settings, val)
-					this.settings.styles = initElementStyles(this.settings.styles)
+					this.element = merge(val, this.element)
 				},
 			},
 		},
 		data() {
 			return {
-				locals: {
-					classType: "column",
-				},
-				settings: {
-					grandParent: 'TemplateBuilder',
-					class: '',
-					hasResizer: true,
-					isActive: true,
-					id: 0,
-					configs: {
-						title: 'Column'
-					},
+				element: {
 					styles: {},
-				},
+				} as IElement,
 			}
 		},
 		methods: {
@@ -52,46 +40,47 @@
 			* Initializing the element utilities for the created element
 			*/
 			initialize() {
-				let elem = new ElementClass(this.$refs.element, `column-${this.settings.id}`, this.settings)
-				elem.clickable()
-				this.resizable()
+				var $el: HTMLElement = this.$refs.element as HTMLElement
+				var resizerQuery: string = `${this.element.type}-${this.element.id}`
+				this.element.makeClickable($el)
+				this.resizable($el, resizerQuery)
 			},
 
 			/**
 			 * Column Private resiable
 			 * @emits {width-changed} call width-changed event and passes startWidth, newWidth, colId.
 			 */
-			resizable() {
-				var element = this.$refs.element
-				const resizers = document.querySelectorAll(`.elem-resizer.column-${this.settings.id}`)
+			resizable(element: HTMLElement, resizerQuery: string): void {
+
+				const resizers: any = document.querySelectorAll(`.elem-resizer.${resizerQuery}`)
 
 				for (let resizer of resizers) {
-					resizer.onmousedown = (e) => initDrag(e, resizer)
+					resizer.onmousedown = (e: any) => initDrag(e, resizer)
 				}
 
-				var startX, startWidth, that = this
-				function initDrag(e, resizer) {
+				var startX: number, startWidth: number
+				const initDrag = (e: any, resizer: HTMLElement) => {
 					if (e.target.className.includes('elem-resizer')) { // If Clicking on the resizer
 						startX = e.clientX
-						startWidth = parseInt(document.defaultView.getComputedStyle(element).width, 10)
+						startWidth = parseInt(document.defaultView!.getComputedStyle(element).width, 10)
 						document.onmousemove = (e) => doDrag(e, resizer)
 						document.onmouseup = stopDrag
 					}
 				}
-				function doDrag(e, resizer) {
+				const doDrag = (e: any, resizer: HTMLElement) => {
 					if (resizer.className.includes('right')) {
 						var newWidth = startWidth + e.clientX - startX
-						that.$emit('width-changed', newWidth, that.settings.id)
+						this.$emit('width-changed', newWidth, this.element.id)
 					}
 				}
 
-				function stopDrag(e) {
+				const stopDrag = (e: any) => {
 					document.onmousemove = null
 					document.onmouseup = null
 				}
 			}
 		},
-	}
+	})
 </script>
 
 <style>
